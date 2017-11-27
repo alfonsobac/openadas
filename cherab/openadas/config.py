4c3ca755093b9cf49cddc6ed5ded6226e5c2a543
Alfonso Baciero <alfonsobac@users.noreply.github.com>
2017-11-27 15:49:41 +0000

    fixed bme rates

--- cherab/openadas/config.py
+++ cherab/openadas/config.py
@@ -95,18 +95,19 @@ default["bmp"][hydrogen][4][neon][10] = "adf22/bmp97#h/bmp97#h_4_ne10.dat"   # H
 
 # beam emission coefficients:
 # config["bme"][beam_species][target_ion][target_ionisation][(initial_level, final_level)] = coefficient
-default["bme"][hydrogen][1][hydrogen][(3, 2)] = "adf22/bme10#h/bme10#h_h1.dat"      # H0(n=3) + H+ -> H0(n=2) + H+ + hv (656.28 nm)
-default["bme"][hydrogen][2][helium][(3, 2)] = "adf22/bme97#h/bme97#h_he2.dat"     # H0(n=3) + He2+ -> H0(n=2) + He2+ + hv (656.28 nm)
-default["bme"][hydrogen][3][lithium][(3, 2)] = "adf22/bme97#h/bme97#h_li3.dat"     # H0(n=3) + Li3+ -> H0(n=2) + Li3+ + hv (656.28 nm)
-default["bme"][hydrogen][4][beryllium][(3, 2)] = "adf22/bme97#h/bme97#h_be4.dat"     # H0(n=3) + Be4+ -> H0(n=2) + Be4+ + hv (656.28 nm)
-default["bme"][hydrogen][5][boron][(3, 2)] = "adf22/bme97#h/bme97#h_b5.dat"      # H0(n=3) + B5+ -> H0(n=2) + B5+ + hv (656.28 nm)
-default["bme"][hydrogen][6][carbon][(3, 2)] = "adf22/bme97#h/bme97#h_c6.dat"      # H0(n=3) + C6+ -> H0(n=2) + C6+ + hv (656.28 nm)
-default["bme"][hydrogen][7][nitrogen][(3, 2)] = "adf22/bme97#h/bme97#h_n7.dat"      # H0(n=3) + N7+ -> H0(n=2) + N7+ + hv (656.28 nm)
-default["bme"][hydrogen][8][fluorine][(3, 2)] = "adf22/bme97#h/bme97#h_f9.dat"      # H0(n=3) + F9+ -> H0(n=2) + F9+ + hv (656.28 nm)
-default["bme"][hydrogen][9][oxygen][(3, 2)] = "adf22/bme97#h/bme97#h_o8.dat"      # H0(n=3) + O8+ -> H0(n=2) + O8+ + hv (656.28 nm)
-default["bme"][hydrogen][10][neon][(3, 2)] = "adf22/bme97#h/bme97#h_ne10.dat"  # H0(n=3) + Ne10+ -> H0(n=2) + Ne10+ + hv (656.28 nm)
-default["bme"][hydrogen][11][argon][(3, 2)] = "adf22/bme99#h/bme99#h_ar18.dat"  # H0(n=3) + Ar18+ -> H0(n=2) + Ar18+ + hv (656.28 nm)
-# default["bme"][hydrogen][12][iron][(3, 2)] = "adf22/bme99#h/bme99#h_fe26.dat"  # H0(n=3) + Fe16+ -> H0(n=2) + Fe26+ + hv (656.28 nm)
+default["bme"][hydrogen][hydrogen][1][(3, 2)] = "adf22/bme10#h/bme10#h_h1.dat"      # H0(n=3) + H+ -> H0(n=2) + H+ + hv (656.28 nm)
+default["bme"][hydrogen][helium][2][(3, 2)] = "adf22/bme97#h/bme97#h_he2.dat"     # H0(n=3) + He2+ -> H0(n=2) + He2+ + hv (656.28 nm)
+default["bme"][hydrogen][lithium][3][(3, 2)] = "adf22/bme97#h/bme97#h_li3.dat"     # H0(n=3) + Li3+ -> H0(n=2) + Li3+ + hv (656.28 nm)
+default["bme"][hydrogen][beryllium][4][(3, 2)] = "adf22/bme97#h/bme97#h_be4.dat"     # H0(n=3) + Be4+ -> H0(n=2) + Be4+ + hv (656.28 nm)
+default["bme"][hydrogen][boron][5][(3, 2)] = "adf22/bme97#h/bme97#h_b5.dat"      # H0(n=3) + B5+ -> H0(n=2) + B5+ + hv (656.28 nm)
+default["bme"][hydrogen][carbon][6][(3, 2)] = "adf22/bme97#h/bme97#h_c6.dat"      # H0(n=3) + C6+ -> H0(n=2) + C6+ + hv (656.28 nm)
+default["bme"][hydrogen][nitrogen][7][(3, 2)] = "adf22/bme97#h/bme97#h_n7.dat"      # H0(n=3) + N7+ -> H0(n=2) + N7+ + hv (656.28 nm)
+default["bme"][hydrogen][oxygen][8][(3, 2)] = "adf22/bme97#h/bme97#h_o8.dat"      # H0(n=3) + O8+ -> H0(n=2) + O8+ + hv (656.28 nm)
+default["bme"][hydrogen][fluorine][9][(3, 2)] = "adf22/bme97#h/bme97#h_f9.dat"      # H0(n=3) + F9+ -> H0(n=2) + F9+ + hv (656.28 nm)
+default["bme"][hydrogen][neon][10][(3, 2)] = "adf22/bme97#h/bme97#h_ne10.dat"  # H0(n=3) + Ne10+ -> H0(n=2) + Ne10+ + hv (656.28 nm)
+default["bme"][hydrogen][argon][18][(3, 2)] = "adf22/bme99#h/bme99#h_ar18.dat"  # H0(n=3) + Ar18+ -> H0(n=2) + Ar18+ + hv (656.28 nm)
+# Iron is not defined yet:
+# default["bme"][hydrogen][iron][26][(3, 2)] = "adf22/bme99#h/bme99#h_fe26.dat"  # H0(n=3) + Fe26+ -> H0(n=2) + Fe26+ + hv (656.28 nm)
 
 
 # beam stopping coefficients:
